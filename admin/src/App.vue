43d5d51cee09e28cf49c0e85043896944d779dd0
xiaguangyu <xiaguangyu@gridsum.com>
2018-11-21 16:37:15 +0800

    vue-cli首次提交

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,42 +1,29 @@
 <template>
   <div id="app">
-    <p class="router-group">
-      <router-link to='/'>To homePage</router-link>
-      <router-link to='/test'>To test</router-link>
-      <router-link to='/bullshit'>To Bullshit</router-link>
-    </p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
 
-<script>
-  import VueQriously from 'vue-qriously';
-  import Vue from 'vue';
-  import './assets/style/iconfont.css'
-  Vue.use(VueQriously);
-  export default {
-    name: 'App'
-  }
-</script>
-
-<style scoped>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
-
-.router-group{
-  position: fixed;
-  bottom: 0;
-  display: flex;
-  width: 100%;
-}
-
-.router-group a{
-  flex: 1
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
